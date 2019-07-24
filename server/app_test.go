360f53c74574f0ad10f82ac54b1eeacaadc40602
Shin dong myung <>
2019-07-24 19:39:54 +0900

    proto buffer test code add

--- server/app_test.go
+++ server/app_test.go
@@ -1,8 +1,25 @@
 package main
 
-todo
+import (
+	"../proto-gen-go"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"log"
+	"testing"
+)
 
+func TestProto1(t *testing.T) {
 
-func main() {
+	test := &tutorial.Person{
+		Name : "a",
+		Id : 10,
+		Email : "realdm99@google.com",
+	}
 
+	data, err := proto.Marshal(test)
+	if (err != nil) {
+		log.Fatal("marsharing error")
+	}
+
+	fmt.Print(data)
 }
