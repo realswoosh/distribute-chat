5a3cd85b30b01b4ff5659fb01cc7a1731866ba73
Shin dong myung <realswoosh@gmail.com>
2019-08-10 13:00:02 +0900

    clientsocket modify

--- client-windows/chat-client-form/Net/ClientSocket.cs
+++ client-windows/chat-client-form/Net/ClientSocket.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace chat_client_form.Net
 {
-	class ClientSocket
+	public sealed class ClientSocket
 	{
+		private static volatile ClientSocket instance;
+		private static object syncRoot = new Object();
+
+		private Socket socket;
+		private const int BufferSize = 1024;
+		private byte[] buffer = new byte[BufferSize];
+
+		private static readonly ManualResetEvent DoneConnect = new ManualResetEvent(false);
+		private static readonly ManualResetEvent DoneSend = new ManualResetEvent(false);
+		private static readonly ManualResetEvent DoneReceive = new ManualResetEvent(false);
+
+		private ClientSocket() { }
+
+		public static ClientSocket Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					lock (syncRoot)
+					{
+						if (instance == null)
+							instance = new ClientSocket();
+					}
+				}
+
+				return instance;
+			}
+		}
+
+		public void Dial(string addr, int port)
+		{
+
+		}
 	}
 }
