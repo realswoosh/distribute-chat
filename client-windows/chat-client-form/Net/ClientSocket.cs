24998b560a1b64f7a2b14ce00c8bc8b8a807cc6d
Shin dong myung <realswoosh@gmail.com>
2019-08-27 23:46:06 +0900

    proto-gen-cs copy to project

--- client-windows/chat-client-form/Net/ClientSocket.cs
+++ client-windows/chat-client-form/Net/ClientSocket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Protobuf;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,6 +20,7 @@ namespace chat_client_form.Net
 		private const int BufferSize = 1024;
 		private byte[] buffer = new byte[BufferSize];
 		private byte[] received = new byte[BufferSize * 8];
+		private int receiveSize = 0;
 
 		private MemoryStream receivedMemoryStream;
 
@@ -91,6 +93,21 @@ namespace chat_client_form.Net
 			}
 		}
 
+		public T ToObject<T>(byte[] buf) where T : IMessage<T>, new()
+		{
+			if (buf == null)
+				return default(T);
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				ms.Write(buf, 0, buf.Length);
+				ms.Seek(0, SeekOrigin.Begin);
+
+				MessageParser<T> parser = new MessageParser<T>(() => new T());
+				return parser.ParseFrom(ms);
+			}
+		}
+
 		private void ReceiveCallback(IAsyncResult ar)
 		{
 
@@ -101,6 +118,39 @@ namespace chat_client_form.Net
 
 				if (bytesRead > 0)
 				{
+					receiveSize += bytesRead;
+
+					receivedMemoryStream.Write(buffer, 0, bytesRead);
+
+					Int32 readOffset = 0;
+
+					byte[] msArray = receivedMemoryStream.ToArray();
+
+					while (Def.Constants.HeaderSize <= receiveSize - readOffset)
+					{
+						int packetSize = BitConverter.ToInt32(msArray, readOffset);
+						if (packetSize >= receiveSize - readOffset)
+						{
+							byte[] packet = new byte[packetSize];
+
+							receivedMemoryStream.Read(packet, 0, packetSize);
+
+							ArraySegment<byte> segMsg = new ArraySegment<byte>(packet, 4, 4);
+							ArraySegment<byte> seg = default;
+
+							if (packetSize > Def.Constants.HeaderSize)
+							{
+								seg = new ArraySegment<byte>(packet, Def.Constants.HeaderSize, packetSize - Def.Constants.HeaderSize);
+							}
+
+							readOffset += packetSize;
+						}
+						else
+						{
+							break;
+						}
+					}
+
 					Console.WriteLine("receive {0} bytes", bytesRead);
 				}
 			}
@@ -108,10 +158,6 @@ namespace chat_client_form.Net
 			{
 
 			}
-
-
 		}
-
-
 	}
 }
